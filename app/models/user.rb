4505149f599ac2a3542f9e06ffe18481cb94132c
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2023-07-12 13:51:18 +0000

    [add] book

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :books, dependent: :destroy
+
+  has_one_attached :profile_image
+
+  def get profile_file_image
+  unless profile_image.attached?
+    file_path = Rails.root.join('app/assets/no_image.jpg')
+    profile_image_attach(io: File.open(file_path))
+  end
+    profile_image.variant(resize_to_limit:[100, 100]).processed
+  end
 end
