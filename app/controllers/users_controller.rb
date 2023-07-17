cc8b99d7e54307f07593438d8f2ae63289902071
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2023-07-17 07:24:32 +0000

    [add]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,30 @@ class UsersController < ApplicationController
     @user = User.all
   end
 
+  def mypage
+    redirect_to user_path(current_user)
+  end
+
   def show
     @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to @user_path
+    end
     @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to @user_path
+    end
+    @user = User.find(params[:id])
+    @user.update
+    redirect_to user_path(@user.id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
 
-  def get_profile_file_image(width, height)
+  def get_profile_image(width, height)
   unless profile_image.attached?
     file_path = Rails.root.join('app/assets/no_image.jpg')
-    profile_image_attach(io: File.open(file_path))
+    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   end
     profile_image.variant(resize_to_limit:[width, height]).processed
   end
