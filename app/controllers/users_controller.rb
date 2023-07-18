0fda1fed401fca7b3cb0a227f27cc8682eaa1496
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2023-07-18 10:40:47 +0000

    [add]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    user = User.find(params[:id])
+    @user = User.find(params[:id])
     unless user.id == current_user.id
       redirect_to @user_path
     end
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
+    @user = User.find(params[:id])
     unless user.id == current_user.id
       redirect_to @user_path
     end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user
 
-  varidates :title, presence: true
-  varidates :body, presence: true, length: {maximum: 200}
+  validates :title, presence: true
+  validates :body, presence: true, length: {maximum: 200}
 end
