718d8ff865f225bf15e6d508ef4598fad1caf39c
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2023-08-01 11:03:47 +0000

    [add]checksheet

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,17 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @user = current_user
+  end
+
   def new
     @book = Book.new
-    @user = current_user
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
   end
 
   def create
@@ -12,19 +22,9 @@ class BooksController < ApplicationController
       redirect_to book_path(@book.id)
     else
       @books = Book.all
-      @user = current_user
+      @user = @book.user
       render :index
     end
-  end
-
-  def index
-    @books = Book.all
-    @user = current_user
-  end
-
-  def show
-    @book = Book.find(params[:id])
-    @user = @book.user
   end
 
   def edit
@@ -53,6 +53,6 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title, :body, :user_id)
+    params.require(:book).permit(:title, :body)
   end
 end
