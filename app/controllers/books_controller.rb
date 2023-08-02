6b3170d27b0a4d78394c8ed0abb1928196be3456
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2023-08-02 12:18:26 +0000

    [add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,8 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
-    @user = current_user
-  end
-
-  def new
     @book = Book.new
+    @user = current_user
   end
 
   def show
