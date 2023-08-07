803100aa7c6777316b1e656208d12f78ecbe537f
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2023-08-07 11:12:26 +0000

    [finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
   def index
     @books = Book.all
     @book = Book.new
@@ -51,5 +52,13 @@ class BooksController < ApplicationController
   private
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    unless @user == current_user
+      redirect_to '/books'
+    end
   end
 end
